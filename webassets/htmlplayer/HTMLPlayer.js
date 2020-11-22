3992dd426eb8c2d54b5580fc4b7e4e27a3f8efa0
Koop <61279093+KoopintheHub@users.noreply.github.com>
2020-11-22 11:45:24 +0000

    Update HTMLPlayer.js

--- webassets/htmlplayer/HTMLPlayer.js
+++ webassets/htmlplayer/HTMLPlayer.js
@@ -1,6 +1,7 @@
 const playerParams = new URLSearchParams(window.location.search);
 
 function bootPlayer() {
+	// Get info of the game
   $.get('./webassets/htmlplayer/gameinfos/' + playerParams.get('name') + '.name', function(name) {
 	  $('#HTMLPlyer_Name').text(name);
   });
